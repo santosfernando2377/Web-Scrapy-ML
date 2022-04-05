46cd27fd4df1d11953d8730194e2d067ab599ac8
Fernando Soares <santosfernando2377@gmail.com>
2022-04-04 22:02:44 -0300

    Foi adicionado o mongoose

--- index.js
+++ index.js
@@ -13,14 +13,15 @@ const readline = require('readline');
         pesquisa()
     });
 })();
-
+const list = []
+let pageInit, pageEnd, y
 const url = 'https://www.mercadolivre.com.br/';
-let search = 'Notebook Gamer';
+let search = '';
 
 //let i = 1;
 
 async function pesquisa() {
-    const browser = await pup.launch({headless: true}); // Instância o navegador
+    const browser = await pup.launch({headless: false}); // Instância o navegador
     const page = await browser.newPage(); // Instância uma nova página
     
     await page.goto(url); // Redireciona para URL
@@ -33,6 +34,16 @@ async function pesquisa() {
         page.waitForNavigation(), 
         page.click('.nav-search-btn') // Clique no elemento
     ]);
+    
+    await page.waitForSelector('.andes-pagination__link');
+    pageInit = await page.$eval('.andes-pagination__link', el => el.innerText); // Recebendo a primeira página
+    
+    await page.waitForSelector('.andes-pagination__page-count');
+    pageEnd = await page.$eval('.andes-pagination__page-count', el => el.innerText); // Recebendo o última página
+
+    y = pageEnd.split(' ')
+    y.shift()
+    pageEnd = Number(y[0]);
 
     const links = await page.$$eval('.ui-search-result__image > a', el => el.map(link => link.href)); // Recebendo os links e mapeando
 
@@ -44,21 +55,27 @@ async function pesquisa() {
         await page.waitForSelector('.ui-pdp-title');
         await page.waitForSelector('.andes-money-amount__fraction');
 
-        const title = await page.$eval('.ui-pdp-title', element => element.innerText);
-        const price = await page.$eval('.andes-money-amount__fraction', element => element.innerText);
+        const Title = await page.$eval('.ui-pdp-title', element => element.innerText);
+        const Price = await page.$eval('.andes-money-amount__fraction', element => element.innerText);
         
-        const Buy = link
+        const Buy = link;
+        const DateSearch = new Date;
 
         const obj = {
-            title,
-            price,
-            Buy
+            Title,
+            Price,
+            Buy,
+            DateSearch
         }
 
-        console.log(obj);
+        list.push(obj);
 
         //i = i + 1;
     }
+
+    //await console.log(list);
+
+    await console.log(list);
 
     await page.waitForTimeout(1000); // Aguardando
 
